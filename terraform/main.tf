0bb539c3f1cbe6c0bb425b92859e59874d79d698
fahamikareem <76165047+fahamikareem@users.noreply.github.com>
2024-10-16 13:12:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,8 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-# Security Group to allow SSH, HTTP, and other ports
+
+# Security Group to allow SSH, HTTP, K8S, and other ports
 resource "aws_security_group" "sg_fkemedicure01" {
   name        = "sg_fkemedicure01"
   description = "Allow SSH, HTTP, and range 8080-8099"
@@ -27,6 +28,14 @@ resource "aws_security_group" "sg_fkemedicure01" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    # Allow K8s API (port 6443)
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -48,16 +57,44 @@ resource "aws_security_group" "sg_fkemedicure01" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-# Create EC2 Instance
-resource "aws_instance" "fkemedicure" {
+# Create EC2 Instance- k8s Master
+resource "aws_instance" "fkemedicure_k8s_master" {
   ami             = "ami-005fc0f236362e99f"
   instance_type = "t2.micro"
   key_name = "ansible_key"
   vpc_security_group_ids = [aws_security_group.sg_fkemedicure01.id]
   tags = {
-    Name = "fkemedicure"
+    Name = "fkemedicure_k8s_master"
   }
 }
-output "fkemedicure_publicip" {
-  value = aws_instance.fkemedicure.public_ip
+# Create EC2 Instance- k8s worker01
+resource "aws_instance" "fkemedicure_k8s_worker01" {
+  ami             = "ami-005fc0f236362e99f"
+  instance_type = "t2.micro"
+  key_name = "ansible_key"
+  vpc_security_group_ids = [aws_security_group.sg_fkemedicure01.id]
+  tags = {
+    Name = "fkemedicure_k8s_worker01"
+  }
+}
+# Create EC2 Instance- k8s worker02
+resource "aws_instance" "fkemedicure_k8s_worker02" {
+  ami             = "ami-005fc0f236362e99f"
+  instance_type = "t2.micro"
+  key_name = "ansible_key"
+  vpc_security_group_ids = [aws_security_group.sg_fkemedicure01.id]
+  tags = {
+    Name = "fkemedicure_k8s_worker02"
+  }
+}
+
+#output
+output "fkemedicure_k8s_master_publicip" {
+  value = aws_instance.fkemedicure_k8s_master.public_ip
+}
+output "fkemedicure_k8s_worker01_publicip" {
+  value = aws_instance.fkemedicure_k8s_worker01.public_ip
+}
+output "fkemedicure_k8s_worker02_publicip" {
+  value = aws_instance.fkemedicure_k8s_worker02.public_ip
 }
